21e4cbb5fa582f49072f1047104df8f7706fc7b3
Pedro henrique Bielik <pbielik@student.42lisboa.com>
2021-07-27 20:10:52 +0100

    UserSchema: pre-Encrypt before save to DB || bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 const Schema = mongoose.Schema;
 
@@ -7,6 +8,33 @@ const UserSchema = new Schema({
 	email: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
 	address: { type: Schema.Types.ObjectId, ref: 'address' } // A relationship with another address schema
+});
+
+// Run the encryption before save to the DB
+UserSchema.pre('save', function(next) {
+	let user = this;
+	// Verify if the password was modified or is newly created then encrypt the password
+	if (this.isModified('password') || this.isNew) {
+		// 1. Generate 10 Long random characters
+		bcrypt.genSalt(10, function(err, salt) {
+			if (err) {
+				return next(err);
+			};
+			// 2. Mix the 10 characters with the user's password
+			// 3. Output the Hash
+			bcrypt.hash(user.password, salt, null, function(err, hash) {
+				if (err) {
+				return next(err);
+				};
+				// 4. set the user password to hash
+				user.password = hash;
+				// 5. Operation is done, move on!
+				next();
+			});
+		});
+	} else {
+		return next();
+	};
 });
 
 // Export the UserSchema - A BluePrint of user object
